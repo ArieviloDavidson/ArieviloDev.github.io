8e6404ac462642ac0f0da332681096fda5ebc45d
Your Name~ <you@example.com>
2022-12-30 21:02:06 -0300

    Spoiler

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var teste = document.querySelector('.card');
+
+function one () {
+    if (teste.style.display === 'none') {
+        teste.style.display = 'flex';
+    } else {
+        teste.style.display = 'none';
+    }
+}

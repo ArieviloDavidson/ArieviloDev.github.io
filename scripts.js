86b2e526dcd04211a9657d4807a7b656add94ac1
Your Name~ <you@example.com>
2023-01-01 21:42:58 -0300

    Surpresinha

--- scripts.js
+++
@@ -1,9 +0,0 @@
-var teste = document.querySelector('.card');
-
-function one () {
-    if (teste.style.display === 'none') {
-        teste.style.display = 'flex';
-    } else {
-        teste.style.display = 'none';
-    }
-}

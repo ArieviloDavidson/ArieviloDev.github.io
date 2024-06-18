035634a645c1cdababc3c97c058024e31526d228
Davidson Oliveira <99026450+ArieviloDavidson@users.noreply.github.com>
2024-06-18 08:30:38 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const loadingElement = document.querySelector('.loading');
-
-setInterval(() => {
-    loadingElement.style.visibility = (loadingElement.style.visibility == 'hidden' ? '' : 'hidden');
-}, 500);
